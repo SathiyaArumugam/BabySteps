08e4260946d18d76a90811b87a7accdc730ec132
Sathiya <sathiyaarumugam@outlook.com>
2020-05-13 17:22:12 +0530

    Basic difference between 2.0 and 3.0

---
+++ Basics.py
@@ -0,0 +1,13 @@
+# Interpreted language
+# Platform independent
+# Free and open-source
+# High level language
+# Simple
+# Embeddable (used within C\C++)
+# Robust
+# Rich Library support
+# Dynamically typed
+# Slow speed of execution
+
+# Difference between Python 2.0 vs 3.0
+
